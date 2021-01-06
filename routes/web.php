be9e8550e85a9fd5cc184e4251c01a48e9cac5e0
Mohamad.mu <mero22.4.1997>
2021-01-06 17:21:39 +0200

    init

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/customers');
 });
 
 Route::get('/customers ',[\App\Http\Controllers\CustomerController::class,'all']);
